8ea397577231914b30d19d99ef65ad1795e05db9
cconstant100 <carloconstant09@aol.com>
2021-03-04 13:13:56 -0500

    project 2 0.1 Carlo Constant

---
+++ curl-scripts/topics/create.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/topics"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+    "topic": {
+      "title": "'"${TITLE}"'",
+      "side1":"'"${SIDE1}"'",
+      "side2": "'"${SIDE2}"'"
+    }
+  }'
+
+echo
